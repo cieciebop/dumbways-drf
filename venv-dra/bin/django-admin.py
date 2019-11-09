893f930e94036a59a07252f6373d73825b071906
cici <ciciziku@gmail.com>
2019-11-09 11:38:21 +0700

    chapter-2

---
+++ venv-dra/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/3EAFF4C905305D72/L/courses/dumbways.id/DRA/venv-dra/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
